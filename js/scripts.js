37c02b3f9c47baf501c2afa96853e659ebdd4b61
MirkoCifelli <ciffo4@hotmail.it>
2023-12-05 15:59:47 +0100

    add color

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,28 @@ for (let i = 1; i<= 100;i++) {
    const divContainer = document.createElement('div');
    divContainer.classList.add('div-interno');
     container.append(divContainer);
-    
+    container.classList.add('d-flex','flex-wrap','gap-3','ms-3');
+    divContainer.classList.add('d-flex','justify-content-center','align-items-center');
 
     if (i % 3 == 0 && i % 5 == 0) {
         divContainer.append('fizzbuzz');
+        divContainer.classList.add('red');
         console.log('fizzbuzz')
     }
     else if (i % 3 == 0) {
         divContainer.append('fizz');
-        console.log('fizz')
+        divContainer.classList.add('green');
+        console.log('fizz');
     }
     else if (i % 5 == 0) {
         divContainer.append('buzz');
-        console.log('buzz')
+        divContainer.classList.add('orange');
+        console.log('buzz');
     }
     else{
         divContainer.append(i);
-        console.log(i)
+        divContainer.classList.add('blue');
+        console.log(i);
     }
 
     
